9ef546b4d91ccbc1d24f461a84d72991a2a2e4af
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-03-18 23:46:19 -0300

    feat: page_login

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+};
+
+export const SpotifyConfiguration = {
+  clientId: 'e3c549b1874b4038b46a653904c39bd4',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    'user-read-currently-playing', // musica tocando agora.
+    'user-read-recently-played', // ler musicas tocadas recentemente
+    'user-read-playback-state', // ler estado do player do usuario
+    'user-top-read', // top artistas e musicas do usuario
+    'user-modify-playback-state', // alterar do player do usuario.
+    'user-library-read', // ler biblioteca dos usuarios
+    'playlist-read-private', // ler playlists privadas
+    'playlist-read-collaborative', // ler playlists colaborativas
+  ],
+};
