ee0f858f9b028a3920a97a9847da80835d2aeeaf
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-03-19 22:48:42 -0300

    feat: check token - get token - acess token

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,8 +9,19 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 export class LoginComponent implements OnInit {
   constructor(private spotifyService: SpotifyService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.checkTokenUrlCallback();
+  }
 
+  //Função que verifica o token retornado do spotify, que salva no LocalStorage e seta na API
+  checkTokenUrlCallback() {
+    const token = this.spotifyService.getTokenUrlCallback();
+    if (!!token) {
+      this.spotifyService.definedAcessToken(token);
+    }
+  }
+
+  //Função que leva para a página de autorização do spotify
   goToPageLogin() {
     window.location.href = this.spotifyService.getUrlLogin();
   }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
 import { SpotifyConfiguration } from 'src/environments/environment';
+import Spotify from 'spotify-web-api-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpotifyService {
-  constructor() {}
+  //variável que armazena a lib do spotify
+  spotifyApi: Spotify.SpotifyWebApiJs = null;
+
+  constructor() {
+    //Criando uma instância da lib do spotify
+    this.spotifyApi = new Spotify();
+  }
 
   getUrlLogin() {
     const authEndpoint = `${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
-    const scopes = `scopes=${SpotifyConfiguration.scopes.join('%20')}&`;
+    const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}&`;
     const responseType = `response_type=token&show_dialog=true`;
     return authEndpoint + clientId + redirectUrl + scopes + responseType;
+  }
+
+  //Função que trata o token retornado do spotify na url
+  getTokenUrlCallback() {
+    console.log(window.location.hash);
+    if (!window.location.hash) {
+      return '';
+    }
+
+    const params = window.location.hash.substring(1).split('&');
+    console.log(params);
+    return params[0].split('=')[1];
+  }
+
+  //função que define o acesso do usuário pelo token
+  definedAcessToken(token: string) {
+    this.spotifyApi.setAccessToken(token);
+    localStorage.setItem('token', token);
   }
 }
