f3df4eb1ef4faea85a593049e83489c8ea0cdfeb
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-03-26 20:45:29 -0300

    fix

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -4,9 +4,10 @@ import { PlayerComponent } from './player.component';
 import { RouterModule } from '@angular/router';
 import { PlayerRoutes } from './player.routes';
 import { LeftPainelComponent } from 'src/app/components/left-painel/left-painel.component';
+import { ButtonMenuComponent } from 'src/app/components/button-menu/button-menu.component';
 
 @NgModule({
-  declarations: [PlayerComponent, LeftPainelComponent],
+  declarations: [PlayerComponent, LeftPainelComponent, ButtonMenuComponent],
   imports: [CommonModule, RouterModule.forChild(PlayerRoutes)],
 })
 export class PlayerModule {}
