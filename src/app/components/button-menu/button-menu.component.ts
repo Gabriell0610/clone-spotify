7074f6e72cf3fa0fad15c23ae9d3b5fdf1f1623c
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-03-29 01:08:56 -0300

    select menu

--- src/app/components/button-menu/button-menu.component.ts
+++ src/app/components/button-menu/button-menu.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button-menu',
   templateUrl: './button-menu.component.html',
   styleUrls: ['./button-menu.component.scss'],
 })
-export class ButtonMenuComponent {}
+export class ButtonMenuComponent implements OnInit {
+  @Input() nameMenu: string = '';
+  @Input() selected: boolean = false;
+  @Output() click = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onClick(): void {}
+}
--- src/app/components/left-painel/left-painel.component.ts
+++ src/app/components/left-painel/left-painel.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-left-painel',
   templateUrl: './left-painel.component.html',
-  styleUrls: ['./left-painel.component.scss']
+  styleUrls: ['./left-painel.component.scss'],
 })
 export class LeftPainelComponent {
+  btnSelected: string = '';
 
+  constructor() {}
+
+  buttonClick(name: string) {
+    this.btnSelected = name;
+  }
 }
